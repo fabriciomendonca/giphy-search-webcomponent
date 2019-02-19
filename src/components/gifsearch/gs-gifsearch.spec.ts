213357ee99fd7026d7c9d11660e9c760f4cd23b9
Fabricio Mendonca Rodrigues <fabriciomendonca@gmail.com>
2019-02-20 00:23:49 +0100

    feat: add main component

---
+++ src/components/gifsearch/gs-gifsearch.spec.ts
@@ -0,0 +1,73 @@
+import { GsGifSearch } from './gs-gifsearch';
+import mocks from '../../utils/mocks.json';
+
+describe('Main component', () => {
+  let component;
+  beforeEach(() => {
+    component = new GsGifSearch();
+  });
+
+  it('creates the component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('renders the component', () => {
+    const vdata = component.render();
+
+    expect(vdata).toBeDefined();
+  });
+
+  it('loads images data', async () => {
+    const spy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve((<any>mocks).data));
+
+    component.componentWillLoad();
+    component.api.getImages = spy;
+
+    await component.loadImages();
+
+    expect(component.gifs).toHaveLength(3);
+  });
+
+  it('runs the onsubmit callback', async () => {
+    const spy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve((<any>mocks).data));
+
+    component.componentWillLoad();
+    component.api.getImages = spy;
+
+    await component.onSubmitSearch({ detail: 'query' });
+
+    expect(component.gifs).toHaveLength(3);
+    expect(component.query).toBe('query');
+  });
+
+  it('runs window onscroll event and updates the list', async () => {
+    const spy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve((<any>mocks).data));
+
+    component.componentWillLoad();
+    component.api.getImages = spy;
+    await component.onWindowScroll();
+
+    expect(component.page).toBe(2);
+    expect(component.gifs).toHaveLength(3);
+  });
+
+  it('runs window onscroll event and does not update the list', async () => {
+    const spy = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve((<any>mocks).data));
+
+    document.documentElement.scrollTop = 10;
+    component.componentWillLoad();
+    component.api.getImages = spy;
+    await component.onWindowScroll();
+
+    expect(component.page).toBe(1);
+    expect(spy).not.toHaveBeenCalled();
+  });
+});
---
+++ src/components/gifsearch/gs-gifsearch.tsx
@@ -0,0 +1,62 @@
+import { Component, Listen, State } from '@stencil/core';
+import { GiphyApiExposer, apiExposer } from '../../utils/api';
+
+@Component({
+  tag: 'gs-gifsearch',
+})
+export class GsGifSearch {
+  private api: GiphyApiExposer;
+  private page = 1;
+
+  @State() gifs = [];
+  @State() query = '';
+
+  componentWillLoad() {
+    this.api = apiExposer('CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6');
+  }
+
+  async loadImages() {
+    let data = await this.api.getImages(this.query, {
+      lang: 'en',
+      limit: 10,
+      offset: this.page - 1,
+      rating: 'g',
+    });
+
+    this.gifs = [...this.gifs, ...data];
+  }
+
+  @Listen('submit-search')
+  async onSubmitSearch(e) {
+    this.gifs = [];
+    this.query = e.detail;
+    await this.loadImages();
+  }
+
+  getBottomValues() {
+    return {
+      offsetHeight: document.documentElement.offsetHeight || 0,
+      scrollTop: document.documentElement.scrollTop || 0,
+      innerHeight: window.innerHeight || 0,
+    };
+  }
+  @Listen('window:scroll')
+  async onWindowScroll() {
+    const { innerHeight, offsetHeight, scrollTop } = this.getBottomValues();
+    const bottom = scrollTop + innerHeight === offsetHeight;
+
+    if (bottom) {
+      this.page += 1;
+      await this.loadImages();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <gs-searchbox />
+        <gs-giflist gifs={this.gifs} />
+      </div>
+    );
+  }
+}
